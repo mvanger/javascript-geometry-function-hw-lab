8fe163401c173363013fd8b48a3ecbfc73e61bbc
mvanger <mvanger@nyu.edu>
2013-07-24 14:41:21 -0400

    Refactored with prototypes

--- geometry.js
+++ geometry.js
@@ -1,63 +1,64 @@
 // Rectangle
-var rectangle = {
-  length: 4,
-  width: 4,
-  isSquare: function(){
-    if (this.length == this.width) {
-      console.log("Is a square");
-    } else {
-      console.log("Is not a square");
-    }
-  },
-  area: function(){
-    console.log(this.length * this.width);
-  },
-  perimeter: function (){
-    console.log(2*this.length + 2*this.width);
+
+function Rectangle(length, width) {
+  this.length = length;
+  this.width = width;
+}
+
+Rectangle.prototype.isSquare = function(){
+  if (this.length == this.width) {
+    console.log("Is a square");
+  } else {
+    console.log("Is not a square");
   }
 };
 
-rectangle.isSquare();
-rectangle.area();
-rectangle.perimeter();
+Rectangle.prototype.area = function(){
+  console.log(this.length * this.width);
+};
+
+Rectangle.prototype.perimeter = function (){
+  console.log(2*this.length + 2*this.width);
+};
+
 
 // Triangle
-var triangle = {
-  sideA: 3,
-  sideB: 4,
-  sideC: 4,
-  isEquilateral: function(){
-    if (this.sideA == this.sideB && this.sideA == this.sideC) {
-      console.log("It is equilateral");
-    } else {
-      console.log("It is not equilateral");
-    }
-  },
-  isIsosceles: function(){
-    if (this.sideA == this.sideB || this.sideA == this.sideC || this.sideB == this.sideC) {
-      console.log("It is isosceles");
-    } else {
-      console.log("It is not isosceles");
-    }
-  },
-  area: function(){
-    var s = (this.sideA + this.sideB + this.sideC)/2;
-    var a = Math.sqrt(s*(s-this.sideA)*(s-this.sideB)*(s-this.sideC));
-    console.log(a);
-  },
-  isObtuse: function(){
-    var a = Math.pow( this.sideA, 2 );
-    var b = Math.pow( this.sideB, 2 );
-    var c = Math.pow( this.sideC, 2 );
-    if ( a+b < c || a+c < b || b+c < a){
-      console.log("It is obtuse");
-    } else {
-      console.log("It is not obtuse");
-    }
+
+function Triangle(sideA, sideB, sideC) {
+  this.sideA = sideA;
+  this.sideB = sideB;
+  this.sideC = sideC;
+}
+
+Triangle.prototype.isEquilateral = function(){
+  if (this.sideA == this.sideB && this.sideA == this.sideC) {
+    console.log("It is equilateral");
+  } else {
+    console.log("It is not equilateral");
   }
 };
 
-triangle.isEquilateral();
-triangle.isIsosceles();
-triangle.area();
-triangle.isObtuse();
+Triangle.prototype.isIsosceles = function(){
+  if (this.sideA == this.sideB || this.sideA == this.sideC || this.sideB == this.sideC) {
+    console.log("It is isosceles");
+  } else {
+    console.log("It is not isosceles");
+  }
+};
+
+Triangle.prototype.area = function(){
+  var s = (this.sideA + this.sideB + this.sideC)/2;
+  var a = Math.sqrt(s*(s-this.sideA)*(s-this.sideB)*(s-this.sideC));
+  console.log(a);
+};
+
+Triangle.prototype.isObtuse = function(){
+  var a = Math.pow( this.sideA, 2 );
+  var b = Math.pow( this.sideB, 2 );
+  var c = Math.pow( this.sideC, 2 );
+  if ( a+b < c || a+c < b || b+c < a){
+    console.log("It is obtuse");
+  } else {
+    console.log("It is not obtuse");
+  }
+};
